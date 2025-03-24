fcd5a00ebb912f66a250c633977ecde76124ef83
Fedor Rudnik <f.rudnik@stroki.dev>
2025-03-24 14:41:32 +0500

    useEffect render cards on HomePage

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,13 +4,7 @@ import className from './Card.module.css'
 
 export const Card = ({card}) => {
     const navigate = useNavigate();
-    const showAnswer = () => {
-        const answer = document.getElementById("answer");
-        answer.style.filter = "none";
-    }
-
-    console.log(card);
-
+    
     return (
         <div className={className.card}>
             <div className={className.badges}>
@@ -18,7 +12,7 @@ export const Card = ({card}) => {
                 <div className={className.badge}>{card.completed ? "Completed" : "Not compeleted"}</div>
             </div>
 
-            <h5 className={className.title}>{card.title}</h5>
+            <h5 className={className.title}>{card.question}</h5>
 
             <div className={className.answerWrapper}>
                 <span>short answer: </span>
